4dd45e9c4c973d8852a7dda343a33e2ecc6318fd
Jorge Blázquez Saborido jorge.jbs@protonmail.com <you@example.com>
2018-06-27 19:45:45 +0200

    Add interpreter for simply typed lambda caluculus

--- src/SimplyTypedLambdaCalculus.hs
+++ src/SimplyTypedLambdaCalculus.hs
@@ -13,6 +13,8 @@ data Ty
 
 data Constant
   = Num Int
+  | Plus2
+  | Plus1 Int
   deriving (Eq, Show)
 
 data Term
@@ -22,27 +24,74 @@ data Term
   | App Term Term
   deriving (Eq, Show)
 
+data AnnTerm
+  = AnnVar Char Ty
+  | AnnConstant Constant Ty
+  | AnnLambda Char AnnTerm Ty
+  | AnnApp AnnTerm AnnTerm Ty
+  deriving (Eq, Show)
+
 constType :: Constant -> Ty
 constType (SimplyTypedLambdaCalculus.Num _) = BaseTy Nat
+constType Plus2 = Arrow (BaseTy Nat) (Arrow (BaseTy Nat) (BaseTy Nat))
+constType (Plus1 _) = Arrow (BaseTy Nat) (BaseTy Nat)
 
-typeCheck :: Term -> Maybe Ty
-typeCheck (Var _ ty) = ty
-typeCheck (Constant c) = Just (constType c)
-typeCheck (Lambda v ty t) = do
-  ty' <- typeCheck (annotate t)
-  return (Arrow ty ty')
+annotatedType :: AnnTerm -> Ty
+annotatedType (AnnVar _ ty) = ty
+annotatedType (AnnConstant _ ty) = ty
+annotatedType (AnnLambda _ _ ty) = ty
+annotatedType (AnnApp _ _ ty) = ty
+
+annotate :: Term -> Maybe AnnTerm
+annotate (Var v ty) = AnnVar v <$> ty
+annotate (Constant c) = Just (AnnConstant c (constType c))
+annotate (Lambda v ty t) = do
+  t' <- annotate (annotate' t)
+  let ty' = annotatedType t'
+  return (AnnLambda v t' (Arrow ty ty'))
   where
-    annotate :: Term -> Term
-    annotate (Var v' _) = Var v' (Just ty)
-    annotate t@(Lambda v' _ t') =
+    annotate' :: Term -> Term
+    annotate' (Var v' _) = Var v' (Just ty)
+    annotate' t@(Lambda v' _ t') =
       if v == v' then
         t
       else
-        annotate t'
-    annotate (App t t') = App (annotate t) (annotate t')
-typeCheck (App t t') = do
-  ty <- typeCheck t
-  ty' <- typeCheck t'
+        annotate' t'
+    annotate' (App t t') = App (annotate' t) (annotate' t')
+    annotate' t = t
+annotate (App t t') = do
+  t'' <- annotate t
+  t''' <- annotate t'
+  let ty = annotatedType t''
+  let ty' = annotatedType t'''
   case ty of
-    Arrow ty1 ty2 | ty1 == ty' -> Just ty2
+    Arrow ty1 ty2 | ty1 == ty' -> Just (AnnApp t'' t''' ty2)
     _ -> Nothing
+
+{- Evaling -}
+
+replace :: AnnTerm -> Char -> AnnTerm -> AnnTerm
+replace t@(AnnVar x _) v t' = if x == v then t' else t
+replace t@(AnnLambda x s ty) v t' =
+  if x == v then
+    t
+  else
+    AnnLambda x (replace s v t') ty
+replace (AnnApp t t' ty) v t'' = AnnApp (replace t v t'') (replace t' v t'') ty
+replace t _ _ = t
+
+beta :: AnnTerm -> AnnTerm
+beta (AnnApp (AnnLambda v t _) t' _) = replace t v t'
+beta (AnnApp t t' ty) = AnnApp (beta t) (beta t') ty
+beta t = t
+
+eval :: AnnTerm -> AnnTerm
+eval (AnnApp t t' ty) = case t of
+    AnnConstant Plus2 _ ->
+      let AnnConstant (SimplyTypedLambdaCalculus.Num x) _ = t'
+      in AnnConstant (Plus1 x) (Arrow (BaseTy Nat) (BaseTy Nat))
+    AnnConstant (Plus1 x) _ ->
+      let AnnConstant (SimplyTypedLambdaCalculus.Num y) _ = t'
+      in AnnConstant (SimplyTypedLambdaCalculus.Num (x+y)) (BaseTy Nat)
+    _ -> eval (beta (AnnApp (eval t) (eval t') ty))
+eval t = t
