72af4502c67434bb1227948074e326d20be2fc10
Jorge Blázquez Saborido jorge.jbs@protonmail.com <you@example.com>
2018-07-08 18:26:00 +0200

    Implement first draft of compiler

--- src/SimplyTypedLambdaCalculus.hs
+++ src/SimplyTypedLambdaCalculus.hs
@@ -2,6 +2,9 @@ module SimplyTypedLambdaCalculus where
 
 import Parse
 
+import Data.List (intersperse)
+import Data.Maybe (fromJust)
+
 data BaseTy
   = Nat
   deriving (Eq, Show)
@@ -95,3 +98,69 @@ eval (AnnApp t t' ty) = case t of
       in AnnConstant (SimplyTypedLambdaCalculus.Num (x+y)) (BaseTy Nat)
     _ -> eval (beta (AnnApp (eval t) (eval t') ty))
 eval t = t
+
+{- Compiling -}
+
+uncurryType :: Ty -> ([Ty], Ty)
+uncurryType (Arrow ty ty') =
+  let p = uncurryType ty'
+  in (ty : fst p, snd p)
+uncurryType ty = ([], ty)
+
+functionParameter :: Ty -> String
+functionParameter (BaseTy Nat) = "int"
+functionParameter (Arrow _ _) = "void*"
+
+functionParameters :: [Ty] -> String
+functionParameters l =
+  concat
+  . intersperse ", "
+  . map (\(ty, i) -> functionParameter ty ++ " arg" ++ show i)
+  . zip l
+  . flip take [0..]
+  $ length l
+
+functionDecl :: Ty -> String -> Maybe String
+functionDecl ty@(Arrow _ _) name =
+  let (parameters, ret) = uncurryType ty
+  in Just (
+    functionParameter ret
+    ++ " "
+    ++ name
+    ++ "(" ++ functionParameters parameters ++ ")"
+  )
+functionDeclr _ = Nothing
+
+{-@ uncurryLambda :: x : AnnTerm -> ([Char], AnnTerm, ([Ty], Ty)) @-}
+uncurryLambda :: AnnTerm -> ([Char], AnnTerm, ([Ty], Ty))
+uncurryLambda (AnnLambda v t ty) = (v : vs, t', uncurryType ty)
+  where
+    (vs, t', _) = uncurryLambda t
+uncurryLambda t = ([], t, ([], annotatedType t))
+
+functionBody :: AnnTerm -> Maybe String
+functionBody t@(AnnLambda _ _ ty) = do
+  let (_, expr, _) = uncurryLambda t
+  (body, bodyName) <- compileTerm expr
+  return
+    ( fromJust (functionDecl ty "hue") ++ "\n"
+      ++ "{" ++ "\n"
+      ++ body
+      ++ "return " ++ bodyName ++ ";" ++ "\n"
+      ++ "}" ++ "\n"
+    )
+
+compileTerm :: AnnTerm -> Maybe (String, String)
+compileTerm (AnnLambda v ty t) = Nothing
+compileTerm (AnnApp t t' _) = do
+  (code, name) <-  compileTerm t
+  (code', name') <- compileTerm t'
+  let newname = "hue"
+  return
+    ( code
+      ++ code'
+      ++ newname ++ " = " ++ name ++ "(" ++ name' ++ ");" ++ "\n"
+    , newname
+    )
+compileTerm (AnnVar v _) = Just ("", v : [])
+compileTerm (AnnConstant c _) = Just ("", show c)
