a108bb96b9b614a2442c2d3fb0c9763686297c92
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-07-25 19:22:05 +0200

    Refactor a bit of the Eval module

--- src/Quesito/Eval.hs
+++ src/Quesito/Eval.hs
@@ -1,15 +1,18 @@
 module Quesito.Eval (eval) where
 
-import Quesito.Constant
+import Quesito.Constant as C
 import Quesito.Expr
 
 replace :: QuesExpr -> Var -> QuesExpr -> QuesExpr
-replace t@(Var x) v t' = if x == v then t' else t
-replace t@(Lambda x ty s) v t' =
-  if x == v then
-    t
-  else
-    Lambda x ty (replace s v t')
+replace (Var u) v t = if u == v then t else Var u
+replace (Lambda u ty r) v s =
+  Lambda u ty
+    (
+      if u == v then
+        r
+      else
+        replace r v s
+    )
 replace (App t t') v t'' = App (replace t v t'') (replace t' v t'')
 replace r@(Let (u, s) t) v p =
   if u == v then
@@ -18,19 +21,15 @@ replace r@(Let (u, s) t) v p =
     Let (u, (replace s v p)) (replace t v p)
 replace t _ _ = t
 
-beta :: QuesExpr -> QuesExpr
-beta (App (Lambda v _ t) t') = replace t v t'
-beta (App t t') = App (beta t) (beta t')
-beta t = t
-
 eval :: QuesExpr -> QuesExpr
+eval (App (Lambda v _ t) t') = eval (replace t v t')
 eval (App t t') = case t of
   Constant Plus2 ->
-    let Constant (Quesito.Constant.Num x) = t'
+    let Constant (C.Num x) = t'
     in Constant (Plus1 x)
   Constant (Plus1 x) ->
-    let Constant (Quesito.Constant.Num y) = t'
-    in Constant (Quesito.Constant.Num (x+y))
-  _ -> eval (beta (App (eval t) (eval t')))
+    let Constant (C.Num y) = t'
+    in Constant (C.Num (x+y))
+  _ -> eval (App (eval t) (eval t'))
 eval (Let (v, t) t') = eval (replace t' v t)
 eval t = t
