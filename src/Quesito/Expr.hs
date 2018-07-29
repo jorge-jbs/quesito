8edb89584fbae1bbd0321e677650bae4077bd9ef
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-07-29 08:46:43 +0200

    Split into inferable terms and checkable terms

--- src/Quesito/Expr.hs
+++ src/Quesito/Expr.hs
@@ -8,77 +8,77 @@ import Control.Monad (unless)
 data Name = Bound String | Binding String | Free String | Ignore
   deriving (Show, Eq)
 
-data Term
+-- | Inferable terms
+data InfTerm
   = Var Name
   | Type Int
-  | Pi Name Term Term
-  | Lam Name Term Term
-  | App Term Term
+  | Pi Name CheckTerm CheckTerm
+  | App InfTerm CheckTerm
+  | Ann CheckTerm CheckTerm
+  deriving (Show, Eq)
+
+-- | Checkable terms
+data CheckTerm
+  = Inf InfTerm
+  | Lam Name CheckTerm
   deriving (Show, Eq)
 
 type Result a = Either String a
 
-type Context = [(Name, Term)]
-type Env = [(Name, Term)]
+type Context = [(Name, CheckTerm)]
+type Env = [(Name, CheckTerm)]
 
-eval :: Env -> Term -> Term
-eval env (Var x) = maybe (Var x) id (snd <$> (find ((==) x . fst) env))
-eval _   (Type lvl) = Type lvl
-eval env (Pi x e e') = Pi x (eval env e) (eval env e')
-eval env (Lam x e e') = Lam x (eval env e) (eval env e')
-eval env (App e e') = case (eval env e, eval env e') of
-  (Lam x _ t, t') -> eval ((x, t') : env) t
-  (Pi x _ t, t') -> eval ((x, t') : env) t
+evalInf :: Env -> InfTerm -> CheckTerm
+evalInf env (Var (Bound x)) =
+  maybe
+    (error ("Bound variable not found: " ++ x))
+    id
+    (snd <$> find ((\y' -> case y' of Binding y | x == y -> True; _ -> False) . fst) env)
+evalInf _   (Var x) = Inf (Var x)
+evalInf _   (Type lvl) = Inf (Type lvl)
+evalInf _   (Pi x e e') = Inf (Pi x e e')
+evalInf env (App e e') = case (evalInf env e, evalCheck env e') of
+  (Lam x t, t') -> evalCheck ((x, t') : env) t
+  (Inf (Pi x _ t), t') -> evalCheck ((x, t') : env) t
   _ -> error "Application to non-function."
+evalInf env (Ann e _) = evalCheck env e
 
-typeInf :: Context -> Term -> Result Term
+evalCheck :: Env -> CheckTerm -> CheckTerm
+evalCheck env (Inf e) = evalInf env e
+evalCheck _   (Lam x e) = Lam x e
+
+typeInf :: Context -> InfTerm -> Result CheckTerm
 typeInf ctx (Var (Bound x)) = case snd <$> find ((\y' -> case y' of Binding y | x == y -> True; _ -> False) . fst) ctx of
   Just t -> Right t
   Nothing -> fail "4"
-typeInf _ (Var x) = Right (Var x)
-typeInf _ (Type i) = Right (Type (i + 1))
-typeInf ctx (Pi x e e') = do
+typeInf _ (Var x) = Right (Inf (Var x))
+typeInf _ (Type i) = Right (Inf (Type (i + 1)))
+typeInf ctx (Pi x (Inf e) (Inf e')) = do
   t <- typeInf ctx e
   case t of
-    Type i -> do
+    Inf (Type i) -> do
       t' <- typeInf ((x, t) : ctx) e'
       case t' of
-        Type j ->
-          return (Type (max i j))
+        Inf (Type j) ->
+          return (Inf (Type (max i j)))
         _ -> fail "1"
     _ -> fail "2"
-typeInf ctx (Lam x ty e) = do
-  t <- typeInf ((x, ty) : ctx) e
-  return (Pi Ignore ty t)
+typeInf _ (Pi _ _ _) = fail ""
 typeInf ctx (App e e') = do
   t <- typeInf ctx e
-  t' <- typeInf ctx e'
   case t of
-    Pi _ r _ | t' == r -> return (eval [] (App t e'))
+    Inf t'@(Pi x r _) -> do
+      typeCheck ((x, t) : ctx) e' r
+      return (evalInf [] (App t' e'))
     _ -> fail "3"
+typeInf ctx (Ann e ty) = do
+  typeCheck ctx e ty
+  return ty
 
--- typeCheck :: Context -> Env -> CheckTerm -> CheckTerm -> Result ()
--- typeCheck ctx env (Lam x e) (Inf (Pi t t')) = do
---   typeCheck ((x, t) : ctx) env e t'
--- typeCheck ctx env (Inf e) t = do
---   t' <- typeInf ctx env e
---   unless (t == t') (fail "")
--- typeCheck _ _ _ _ = fail ""
-
--- typeCheck :: Term -> Context -> Term -> Result ()
--- typeCheck (Var x) d ty = do
---   let ty' = snd <$> find ((==) x . fst) d
---   if ty' == Just ty then
---     return ()
---   else
---     fail ""
--- typeCheck (Type i) _ (Type j) | i == j    = return ()
---                               | otherwise = fail ""
--- typeCheck (Pi r r') d (Pi t t') = do
---   let s = eval r d
---   let s' = eval r' d
---   if s == t && s' == t' then
---     return ()
---   else
---     fail ""
--- --typeCheck (Pi r r') d (Pi t t') = do
+typeCheck :: Context -> CheckTerm -> CheckTerm -> Result ()
+typeCheck ctx (Lam x e) (Inf pi_@(Pi _ t _)) =
+  typeCheck ((x, t) : ctx) e (evalInf [] (App pi_ (Inf (Var x))))
+typeCheck _ (Lam _ _) _ = fail "6"
+typeCheck ctx (Inf t) ty = do
+  ty' <- typeInf ctx t
+  unless (ty == ty') (fail "5")
